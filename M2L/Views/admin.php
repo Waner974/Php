8adcf452391eea1522938bc3da48eb10e955e522
Grey <Grey@192.168.35.229>
2017-03-01 19:48:24 +0100

    Modification des modals pour l'admin

--- M2L/Views/admin.php
+++ M2L/Views/admin.php
@@ -7,14 +7,17 @@
                 <?= statsAdmin::stats("purple","$nbPresta","Prestataires","person-add","presta") ?>
                 <?= statsAdmin::stats("blue","$nbForm","Formations","stats-bars","form") ?>
 
-<div class="modal fade" id="admin" role="dialog">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header">
-             <button type="button" class="close" data-dismiss="modal">&times;</button>
-             <h4 class="modal-title">Liste des administrateurs</h4>
-             </div>
-             <div class="modal-body">
+
+    <div class="example-modal">
+        <div class="modal" id="admin">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Liste des administrateurs</h4>
+              </div>
+              <div class="modal-body">
                  <table class="table table-hover">
                  <thead>
                  <tr>
@@ -38,18 +41,29 @@
                  ?>
                    </table>  
             </div>
-        </div> 
-    </div>
-</div>
-
-<div class="modal fade" id="chef" role="dialog">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header">
-             <button type="button" class="close" data-dismiss="modal">&times;</button>
-             <h4 class="modal-title">Liste chefs</h4>
-             </div>
-             <div class="modal-body">
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+            <!-- /.modal-content -->
+          </div>
+          <!-- /.modal-dialog -->
+        </div>
+        <!-- /.modal -->
+      </div>
+      <!-- /.example-modal -->
+                
+    <div class="example-modal">
+        <div class="modal" id="chef">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Liste chefs</h4>
+              </div>
+              <div class="modal-body">
                  <table class="table table-hover">
                  <thead>
                  <tr>
@@ -73,18 +87,29 @@
                  ?>
                    </table>  
             </div>
-        </div> 
-    </div>
-</div>
-
-<div class="modal fade" id="user" role="dialog">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header">
-             <button type="button" class="close" data-dismiss="modal">&times;</button>
-             <h4 class="modal-title">Liste des utilisateurs</h4>
-             </div>
-             <div class="modal-body">
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+            <!-- /.modal-content -->
+          </div>
+          <!-- /.modal-dialog -->
+        </div>
+        <!-- /.modal -->
+      </div>
+      <!-- /.example-modal --> 
+                
+    <div class="example-modal">
+        <div class="modal" id="user">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Liste des utilisateurs</h4>
+              </div>
+              <div class="modal-body">
                  <table class="table table-hover">
                  <thead>
                  <tr>
@@ -108,18 +133,29 @@
                  ?>
                    </table>  
             </div>
-        </div> 
-    </div>
-</div>
-
-<div class="modal fade" id="presta" role="dialog">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header">
-             <button type="button" class="close" data-dismiss="modal">&times;</button>
-             <h4 class="modal-title">Liste des prestataires</h4>
-             </div>
-             <div class="modal-body">
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+            <!-- /.modal-content -->
+          </div>
+          <!-- /.modal-dialog -->
+        </div>
+        <!-- /.modal -->
+      </div>
+      <!-- /.example-modal -->
+                
+    <div class="example-modal">
+        <div class="modal" id="presta">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Liste des prestataires</h4>
+              </div>
+              <div class="modal-body">
                  <table class="table table-hover">
                  <thead>
                  <tr>
@@ -139,18 +175,29 @@
                  ?>
                    </table>  
             </div>
-        </div> 
-    </div>
-</div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+            <!-- /.modal-content -->
+          </div>
+          <!-- /.modal-dialog -->
+        </div>
+        <!-- /.modal -->
+      </div>
+      <!-- /.example-modal -->
 
-<div class="modal fade" id="form" role="dialog">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header">
-             <button type="button" class="close" data-dismiss="modal">&times;</button>
-             <h4 class="modal-title">Liste des formations</h4>
-             </div>
-             <div class="modal-body">
+    <div class="example-modal">
+        <div class="modal" id="form">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Liste des formations</h4>
+              </div>
+              <div class="modal-body">
                  <table class="table table-hover">
                  <thead>
                  <tr>
@@ -172,9 +219,19 @@
                  ?>
                    </table>  
             </div>
-        </div> 
-    </div>
-</div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
             </div>
+            <!-- /.modal-content -->
+          </div>
+          <!-- /.modal-dialog -->
+        </div>
+        <!-- /.modal -->
+      </div>
+      <!-- /.example-modal -->
+                
+        </div>
     </div>
 </div>
