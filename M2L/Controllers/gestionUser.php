0229ac6a743c8ebe62a1334d3cb8f19c0553c243
Thomasevano <thomas-evano@hotmail.fr>
2017-03-19 18:07:36 +0100

    Ajout de l'envoi de mail avec ses identifiants a l'utilisateur lors de l'inscription par un admin et un chef (pour pouvoir envoyer des mails il vous faut sendmail)

--- M2L/Controllers/gestionUser.php
+++ M2L/Controllers/gestionUser.php
@@ -5,7 +5,37 @@
         if(isset($_POST['submit']))
         {
             addUser();
-                
+
+            $header="MIME-Version: 1.0\r\n";
+            $header.='From:"M2L-Formation.com"<support@m2lformation.com>'."\n";
+            $header.='Content-Type:text/html; charset="uft-8"'."\n";
+            $header.='Content-Transfer-Encoding: 8bit';
+
+            $nom = $_POST['nom'];
+            $prenom = $_POST['prenom'];
+            $mail = $_POST['mail'];
+            $password = $_POST['mdp'];
+            $nbjour = $_POST['NbJour'];
+            $credits = $_POST['credits'];
+
+            $message='
+            <html>
+                <body>
+                    <div align="center">
+                        <br />
+                        Un Administrateur vous as inscrit a M2L formation<br />
+                        Voici vos Identifants:<br />
+                        Nom: <strong>'.$nom.'</strong><br />
+                        Prenom: <strong>'.$prenom.'</strong><br />
+                        Mail: <strong>'.$mail.'</strong><br />
+                        Mot de Passe: <strong>'.$password.'</strong><br />
+                        Vous disposez de : <strong>'.$nbjour.'</strong> jours de formation et de <strong>'.$credits.'</strong> credits<br />
+                        <br />
+                    </div>
+                </body>
+            </html>
+            ';
+            mail($mail, "Inscription M2L formation", $message, $header);
             
         }
         $_GET['p'] = 'admin';
@@ -17,6 +47,37 @@
         if(isset($_POST['submit']))
         {
             addUserPourChef();
+
+            $header="MIME-Version: 1.0\r\n";
+            $header.='From:"M2L-Formation.com"<support@m2lformation.com>'."\n";
+            $header.='Content-Type:text/html; charset="uft-8"'."\n";
+            $header.='Content-Transfer-Encoding: 8bit';
+
+            $nom = $_POST['nom'];
+            $prenom = $_POST['prenom'];
+            $mail = $_POST['mail'];
+            $password = $_POST['mdp'];
+            $nbjour = $_POST['NbJour'];
+            $credits = $_POST['credits'];
+
+            $message='
+            <html>
+                <body>
+                    <div align="center">
+                        <br />
+                        Un Chef d\'equipe vous as inscrit a M2L formation<br />
+                        Voici vos Identifants:<br />
+                        Nom: <strong>'.$nom.'</strong><br />
+                        Prenom: <strong>'.$prenom.'</strong><br />
+                        Mail: <strong>'.$mail.'</strong><br />
+                        Mot de Passe: <strong>'.$password.'</strong><br />
+                        Vous disposez de : <strong>'.$nbjour.'</strong> jours de formation et de <strong>'.$credits.'</strong> credits<br />
+                        <br />
+                    </div>
+                </body>
+            </html>
+            ';
+            mail($mail, "Inscription M2L formation", $message, $header);
         }
         $_GET['p'] = 'chef';
         require "Views/gestionUser.php";
